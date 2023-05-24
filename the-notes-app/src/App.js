e29e8ca81e4d8559bdca3ff38c1c03df8fb74363
Yagna Amballa <yagna.s.amballa@kipi.bi>
2023-05-24 20:17:50 +0530

    added the lazy state load functionality for the state so it doesn't fetch from the local storage everytime any of the states gets updated

--- the-notes-app/src/App.js
+++ the-notes-app/src/App.js
@@ -9,7 +9,9 @@ import "react-mde/lib/styles/css/react-mde-all.css"
 
 
 export default function App() {
-    const [notes, setNotes] = React.useState(JSON.parse(localStorage.getItem("notes"))|| [])
+    const [notes, setNotes] = React.useState(
+        ()=> JSON.parse(localStorage.getItem("notes"))|| []
+        )
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
